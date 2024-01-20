02a7e76fc7266d16cc92116fd1fea60d53fe2a98
Andres Fandos Villanueva <71872419+FandosA@users.noreply.github.com>
2024-01-20 17:50:58 +0100

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 14:41:21 2023
+
+@author: afandos
+"""
+
+import os
+import json
+
+dataset_path = "dataset/train"
+dataset_output_file = "dataset_autoencoder.json"
+scenearios = os.listdir(dataset_path)
+
+data_autoencoder = {
+    "frame_ant": [],
+    "frame": [],
+    "flow": []
+}
+        
+for i, sceneario in enumerate(scenearios):
+    
+    images_path = os.path.join(dataset_path, sceneario, "image_left")
+    images_names = os.listdir(images_path)
+    optical_flow_path = os.path.join(dataset_path, sceneario, "flow")
+    flow_files = os.listdir(optical_flow_path)
+    
+    for i in range(len(images_names) - 1):
+        
+        data_autoencoder["frame_ant"].append(os.path.join(images_path, images_names[i]))
+        data_autoencoder["frame"].append(os.path.join(images_path, images_names[i + 1]))
+        
+        data_autoencoder["flow"].append(os.path.join(optical_flow_path, flow_files[2 * i]))
+
+# Save data in json file
+with open(dataset_output_file, "w") as fp:
+    json.dump(data_autoencoder, fp, indent=4)
