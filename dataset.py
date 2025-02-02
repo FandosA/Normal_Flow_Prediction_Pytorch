e6bb817a0081fbec25dc97db596147186e6bbbad
Andres Fandos Villanueva <71872419+FandosA@users.noreply.github.com>
2025-02-02 16:32:14 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,31 +7,37 @@ Created on Mon Oct 23 14:41:21 2023
 
 import os
 import json
+import configargparse
 
-dataset_path = "dataset/train"
-dataset_output_file = "dataset_autoencoder.json"
-scenearios = os.listdir(dataset_path)
 
-data_autoencoder = {
-    "frame_ant": [],
-    "frame": [],
-    "flow": []
-}
-        
-for i, sceneario in enumerate(scenearios):
+if __name__ == "__main__":
     
-    images_path = os.path.join(dataset_path, sceneario, "image_left")
-    images_names = os.listdir(images_path)
-    optical_flow_path = os.path.join(dataset_path, sceneario, "flow")
-    flow_files = os.listdir(optical_flow_path)
+    arg = configargparse.ArgumentParser()
+    arg.add_argument('--dataset_path', type=str, default='dataset/train', help='Dataset path.')
+    args = arg.parse_args()
     
-    for i in range(len(images_names) - 1):
+    scenearios = os.listdir(args.dataset_path)
+    
+    data_autoencoder = {
+        "frame_ant": [],
+        "frame": [],
+        "optical_flow": []
+    }
+            
+    for i, sceneario in enumerate(scenearios):
         
-        data_autoencoder["frame_ant"].append(os.path.join(images_path, images_names[i]))
-        data_autoencoder["frame"].append(os.path.join(images_path, images_names[i + 1]))
+        images_path = os.path.join(args.dataset_path, sceneario, "image_left")
+        images_names = os.listdir(images_path)
         
-        data_autoencoder["flow"].append(os.path.join(optical_flow_path, flow_files[2 * i]))
-
-# Save data in json file
-with open(dataset_output_file, "w") as fp:
-    json.dump(data_autoencoder, fp, indent=4)
+        optical_flow_path = os.path.join(args.dataset_path, sceneario, "flow")
+        flow_files = os.listdir(optical_flow_path)
+        
+        for i in range(len(images_names) - 1):
+            
+            data_autoencoder["frame_ant"].append(os.path.join(images_path, images_names[i]))
+            data_autoencoder["frame"].append(os.path.join(images_path, images_names[i + 1]))
+            data_autoencoder["optical_flow"].append(os.path.join(optical_flow_path, flow_files[2 * i]))
+    
+    # Save data in json file
+    with open("dataset_paths.json", "w") as fp:
+        json.dump(data_autoencoder, fp, indent=4)
